93610269a2e8b33659d52490d01422ce9366a22e
Arabkhon-enginner <arabkhon2322@gmail.com>
2023-06-13 09:52:52 +0300

    add text

--- dars.js
+++ dars.js
@@ -1,4 +1,4 @@
-let birinchi_raqam = 12;
+/*let birinchi_raqam = 12;
 
 let ikkinchi_raqam = 20;
 
@@ -7,3 +7,26 @@ let ayir= birinchi_raqam - ikkinchi_raqam
 
 console.log(qosh);
 console.log(ayir);
+*/
+
+/* javascript web browserda ishlaydigan til xisoblanadi. 
+o'zgaruvchilarni kalit so'zlar bilan yaratiladi. kalit so'zlar 3ta
+var, let const
+shu kalit so'zlar ishlatilmasa o'zgaruvchi xosil bo'lmaydi.
+o'zgaruvchilarni "declare" va kalit sozga qiymat beriladi. qiymatni initilisation
+deyiladi. 
+var, let const kalit so'zlari oladigan xossalari jihatdan farq qiladi.
+scope yoki qavs turlari 3ga bolinadi. global scope, local yoki function scope, block scope
+var function scope turiga kiradi. tashqaridan chaqirilganda undefined yoki qiymat qaytradi
+ES 6 yoki 2015 yilgacha var ishlatilgan.
+berilgan o'zgaruvchini qiymatini qaytadan o'zgartirish mumkin
+
+
+*/
+
+if(true){
+    var arry = 12;
+    arry = 25
+    arry = "salom"
+}
+console.log(arry);
--- dars.py
+++ dars.py
@@ -1,8 +1,16 @@
-fnum = 12;
-lnum =20;
+# fnum = 12;
+# lnum =20;
 
-qoshuv = fnum + lnum
-ayir = fnum - lnum
+# qoshuv = fnum + lnum;
+# ayir = fnum - lnum;
+# kop = fnum * lnum;
+# boluv = fnum / lnum;
 
-print(qoshuv)
-print(ayir)
+# print(qoshuv)
+# print(ayir)
+# print(kop)
+# print(boluv)
+
+txt = "men shamsiyev baxodir";
+upper = txt.upper()
+print(upper)
